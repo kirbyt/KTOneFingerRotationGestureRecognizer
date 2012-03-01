bd7199fd1c623b9d8f22c08df0878da7d2bba38c
Kirby Turner <kirby@thecave.com>
2012-03-01 04:01:18 -0500

    Added licensing comment header.

--- KnobSample/KnobSample/ViewController.m
+++ KnobSample/KnobSample/ViewController.m
@@ -1,10 +1,29 @@
-//
-//  ViewController.m
-//  KnobSample
-//
-//  Created by Kirby Turner on 3/1/12.
-//  Copyright (c) 2012 White Peak Software Inc. All rights reserved.
-//
+/**
+ **   KnobSample
+ **
+ **   Created by Kirby Turner.
+ **   Copyright (c) 2012 White Peak Software. All rights reserved.
+ **
+ **   Permission is hereby granted, free of charge, to any person obtaining 
+ **   a copy of this software and associated documentation files (the 
+ **   "Software"), to deal in the Software without restriction, including 
+ **   without limitation the rights to use, copy, modify, merge, publish, 
+ **   distribute, sublicense, and/or sell copies of the Software, and to permit 
+ **   persons to whom the Software is furnished to do so, subject to the 
+ **   following conditions:
+ **
+ **   The above copyright notice and this permission notice shall be included 
+ **   in all copies or substantial portions of the Software.
+ **
+ **   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS 
+ **   OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF 
+ **   MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. 
+ **   IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY 
+ **   CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, 
+ **   TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE 
+ **   SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ **
+ **/
 
 #import "ViewController.h"
 #import "KTOneFingerRotationGestureRecognizer.h"
@@ -29,27 +48,16 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
    
    KTOneFingerRotationGestureRecognizer *spin = [[KTOneFingerRotationGestureRecognizer alloc] initWithTarget:self action:@selector(rotated:)];
    [[self knobImageView] addGestureRecognizer:spin];
    [[self knobImageView] setUserInteractionEnabled:YES];
 
+   // Allow rotation between the start and stop angles.
    [self setStartAngle:90.0];
-   [self setStopAngle:360.0];
+   [self setStopAngle:270.0];
    
    [self resetKnob:self];
-}
-
-- (void)viewDidUnload
-{
-    [super viewDidUnload];
-    // Release any retained subviews of the main view.
-}
-
-- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
-{
-   return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
 }
 
 - (void)rotated:(KTOneFingerRotationGestureRecognizer *)recognizer
